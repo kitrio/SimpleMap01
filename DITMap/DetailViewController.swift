6ff94750b714a71f7fd7555dfaa80de868cae739
D7703_17 <d7703_17@D7703-17ui-iMac.local>
2017-09-14 16:40:32 +0900

    파일 수정

--- DITMap/DetailViewController.swift
+++ DITMap/DetailViewController.swift
@@ -17,7 +17,7 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print(title_name)
-      
+        title = title_name
       
       
 //      ---------------------------------------------------------
@@ -25,13 +25,19 @@ class DetailViewController: UIViewController {
       case "DIT 동의과학대학교":
             ImgView.image = UIImage(named: "img_dit.jpg")
       case "부산시민공원":
-            ImgView.image = UIImage(named: "ditLogo.jpg")
+            ImgView.image = UIImage(named: "bupark.png0")
       default:
             print("default : \(title_name)")
       }
       
       
-      
+      //      ---------------------------------------------------------
+      //      if title_name == "DIT 동의과학대학교" {
+      //            ImgView.image = UIImage(named: "img_dit.jpg")
+      //      }else if title_name == "부산시민공원"{
+      //
+      //      }
+      //      ---------------------------------------------------------
       
 
         // Do any additional setup after loading the view.
@@ -41,9 +47,4 @@ class DetailViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-      
-
-    
-
 }
