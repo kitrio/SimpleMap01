8ae750c0d1c987d209db50f8278969382b9f45bb
D7703_17 <d7703_17@D7703-17ui-iMac.local>
2017-09-14 15:09:32 +0900

    Initial Commit

---
+++ DITMap/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  DITMap
+//
+//  Created by D7703_17 on 14/09/2017.
+//  Copyright © 2017 김종현. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+      @IBOutlet weak var ImgView: UIImageView!
+      
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+      
+
+    
+
+}
